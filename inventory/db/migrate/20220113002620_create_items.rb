4731d99213000e1335f578be2f90f8f7a4826b4a
MoonisMohammad <67934502+MoonisMohammad@users.noreply.github.com>
2022-01-14 20:51:48 -0500

    the first push of inventory files

---
+++ inventory/db/migrate/20220113002620_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration[7.0]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.text :category
+      t.integer :quantity
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
